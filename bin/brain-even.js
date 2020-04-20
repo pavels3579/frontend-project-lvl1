840c875214999bbd09c2ddd4ac26b622abc6625e
pavels3579 <pavels357@yandex.ru>
2020-04-21 00:38:54 +0400

    step 5. Brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,25 +1,34 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
+import getName from '../src/get-name.js';
 
+const name = `${getName()}`;
 const count = 3;
 const question = 'Question: ';
 const correct = 'Correct!';
-const congradulations = 'Congradulations, Bill!';
-const tryAgain = 'Let\s try again, Bill!';
+const congradulations = `Congradulations, ${name}!`;
+const tryAgain = `Let\s try again, ${name}!`;
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 let isWinner = true;
+let correctAnswer = 'yes';
 
 for (let i = 0; i < count; i += 1) {
-  const randomDigit = Math.floor(Math.random() * 10);
+  const randomDigit = Math.floor(Math.random() * 10) + 1;
   console.log(`${question}`, randomDigit.toString());
   const answer = readlineSync.question('Your answer: ');
 
-  if (randomDigit % 2 === 0 && answer === 'yes' || randomDigit % 2 !== 0 && answer === 'no')
+  if (randomDigit % 2 === 0) {
+    correctAnswer = 'yes';
+  } else {
+    correctAnswer = 'no';
+  }
+
+  if (answer === correctAnswer) {
     console.log(correct);
   } else {
-    console.log(`${answer} is wrong answer ;(. Correct answer is `);
+    console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}`);
     isWinner = false;
     break;
   }
