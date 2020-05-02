18955a2ddeefd61b7fc1c566b256b645b48aa9c2
pavels3579 <pavels357@yandex.ru>
2020-05-03 02:02:03 +0400

    change brain-gcd

--- games/brain-gcd.js
+++ games/brain-gcd.js
@@ -1,28 +1,19 @@
 #!/usr/bin/env node
 import { roundsCount, main } from '../src/index.js';
 import getRandomNumber from '../src/get-random-number.js';
-import getLowerNumber from '../src/get-lower-number.js';
+import getGcdTwoNumbers from '../src/get-gcd-two-numbers.js';
 
 const playBrainGcd = () => {
   const rule = 'Find the greatest common divisor of given numbers.';
   let correctAnswer;
   const coll = [];
-  let min;
 
   for (let i = 0; i < roundsCount; i += 1) {
-    const firstElement = getRandomNumber(10);
-    const secondElement = getRandomNumber(10);
-    correctAnswer = 1;
+    const firstNumber = getRandomNumber(10) + 1;
+    const secondNumber = getRandomNumber(10) + 1;
+    correctAnswer = getGcdTwoNumbers(firstNumber, secondNumber);
 
-    min = getLowerNumber(firstElement, secondElement);
-
-    for (let e = 1; e <= min; e += 1) {
-      if (firstElement % e === 0 && secondElement % e === 0) {
-        correctAnswer = e;
-      }
-    }
-
-    const task = `${firstElement.toString()} ${secondElement.toString()} `;
+    const task = `${firstNumber.toString()} ${secondNumber.toString()} `;
     coll.push([task, correctAnswer.toString()]);
   }
 
